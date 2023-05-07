b849c5d6ea42535e5718a1bbe7431a9a3b60bde2
ChoMinGi <c9010912@gmail.com>
2023-05-07 23:34:42 +0900

    RecyclerView 발표용으로 변경

--- app/src/main/java/com/example/ibda_frontend/ReActivity.kt
+++ app/src/main/java/com/example/ibda_frontend/ReActivity.kt
@@ -12,27 +12,27 @@ class ReActivity : AppCompatActivity() {
         setContentView(R.layout.activity_recyclerview)
 
         val profileList = arrayListOf(
-            Profiles(R.drawable.man, "김철수", 25,"자바 개발자"),
-            Profiles(R.drawable.woman, "김철수", 25,"자바 개발자"),
-            Profiles(R.drawable.woman, "김철수", 25,"자바 개발자"),
-            Profiles(R.drawable.man, "김철수", 25,"자바 개발자"),
-            Profiles(R.drawable.woman, "김철수", 25,"자바2 개발자"),
-            Profiles(R.drawable.man, "김철다", 25,"자바3 개발자"),
-            Profiles(R.drawable.woman, "김철니", 25,"자바 개발자"),
-            Profiles(R.drawable.man, "김철습", 25,"자바5 개발자"),
-            Profiles(R.drawable.man, "김철갑", 25,"자바8 개발자"),
-            Profiles(R.drawable.man, "김철반", 25,"자바 개발자"),
-            Profiles(R.drawable.woman, "김철여", 25,"자바 개발자"),
-            Profiles(R.drawable.man, "김철세", 25,"자바0 개발자"),
-            Profiles(R.drawable.man, "김철하", 25,"자바 개발자"),
-            Profiles(R.drawable.woman, "김철녕", 25,"자바 개발자"),
-            Profiles(R.drawable.man, "김철안", 25,"자바 개발자"),
-            Profiles(R.drawable.man, "김철이", 25,"자바11 개발자"),
-            Profiles(R.drawable.man, "김철뭐", 25,"자바 개발자"),
-            Profiles(R.drawable.man, "김철시", 25,"자바23 개발자"),
-            Profiles(R.drawable.man, "김철수", 25,"자바 개발자"),
-            Profiles(R.drawable.man, "김철수", 25,"자바44 개발자"),
-            Profiles(R.drawable.woman, "김수아", 25,"자바 개발자")
+            Profiles(R.drawable.man, "김철수1", 25,"자바 개발자"),
+            Profiles(R.drawable.woman, "김철수2", 25,"자바 개발자"),
+            Profiles(R.drawable.woman, "김철수3", 25,"자바 개발자"),
+            Profiles(R.drawable.man, "김철수4", 25,"자바 개발자"),
+            Profiles(R.drawable.woman, "김철수5", 25,"자바2 개발자"),
+            Profiles(R.drawable.man, "김철수6", 25,"자바3 개발자"),
+            Profiles(R.drawable.woman, "김철수7", 25,"자바 개발자"),
+            Profiles(R.drawable.man, "김철수8", 25,"자바5 개발자"),
+            Profiles(R.drawable.man, "김철수9", 25,"자바8 개발자"),
+            Profiles(R.drawable.man, "김철수10", 25,"자바 개발자"),
+            Profiles(R.drawable.woman, "김철수11", 25,"자바 개발자"),
+            Profiles(R.drawable.man, "김철수12", 25,"자바0 개발자"),
+            Profiles(R.drawable.man, "김철수13", 25,"자바 개발자"),
+            Profiles(R.drawable.woman, "김철수14", 25,"자바 개발자"),
+            Profiles(R.drawable.man, "김철수15", 25,"자바 개발자"),
+            Profiles(R.drawable.man, "김철수16", 25,"자바11 개발자"),
+            Profiles(R.drawable.man, "김철수17", 25,"자바 개발자"),
+            Profiles(R.drawable.man, "김철수18", 25,"자바23 개발자"),
+            Profiles(R.drawable.man, "김철수19", 25,"자바 개발자"),
+            Profiles(R.drawable.man, "김철수20", 25,"자바44 개발자"),
+            Profiles(R.drawable.woman, "김철수21", 25,"자바 개발자")
         )
         val rvProfile = findViewById<RecyclerView>(R.id.rv_profile)
         rvProfile.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false)
